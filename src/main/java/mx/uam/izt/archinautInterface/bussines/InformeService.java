8dbc69d1e995af56543715256370711c9a3f0536
ErickESC <eekoston@gmail.com>
2021-01-15 07:18:09 -0600

    Ends-Point almost conected

--- src/main/java/mx/uam/izt/archinautInterface/bussines/InformeService.java
+++ src/main/java/mx/uam/izt/archinautInterface/bussines/InformeService.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import lombok.extern.slf4j.Slf4j;
