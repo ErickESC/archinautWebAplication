d95d3d08676938a36418fd1db0bcca9ed25421c1
ErickESC <eekoston@gmail.com>
2021-07-30 14:52:29 -0500

    All standarized

--- src/main/java/mx/uam/izt/archinautInterface/services/MongoDBController.java
+++ src/main/java/mx/uam/izt/archinautInterface/services/MongoDBController.java
@@ -7,8 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.ApiOperation;
@@ -24,23 +22,8 @@ public class MongoDBController {
 	MongoDBRepository mongoRepository;
 	
 	@ApiOperation(
-			value = "Recibe json",
-			notes = "Recibe un json del analysis"
-			)
-	@PostMapping(path = "/mongo", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity <?> create(@RequestBody AnalysisResult archResult) {
-		
-		log.info("Analysis: " + archResult);
-		
-		mongoRepository.save(archResult);
-		
-		return ResponseEntity.status(HttpStatus.OK).body(archResult);
-		
-	}
-	
-	@ApiOperation(
-			value = "Regresa todos los datos ya procesados para Depens",
-			notes = "Regresa un json con una lista de los datos procesados"
+			value = "Retrieves all the reports saved in the DB",
+			notes = "Retrieves a Json with a list of the reports"
 			)
 	@GetMapping(path = "/mongo/retrieve", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity <?> retrieveAll() {
